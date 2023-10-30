fa8931cebb8aa2f984ab5008ad46c2f5eef021cc
Emer Connelly <emer.connelly@wolterskluwer.com>
2023-10-29 21:37:33 -0500

    commit

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Emerson")
-	want := "Hello, Emerson"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Emerson")
+		want := "Hello, Emerson"
 
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("emtpy string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
